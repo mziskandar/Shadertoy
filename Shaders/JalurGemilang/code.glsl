6d7f9b0d5a910cc3487335d23405bb77f99eea08
mziskandar <mziskandar@yahoo.com>
2021-08-28 16:14:25 +0800

    Update code.glsl

--- Shaders/JalurGemilang/code.glsl
+++ Shaders/JalurGemilang/code.glsl
@@ -8,6 +8,7 @@
 
 #define PI 3.14159265359
 #define TAU (2. * PI)
+#define BlueHeight (1.0 - ((1.0 / 14.0) * 8.0))
 
 vec3 RED = vec3(204.0/255.0, 0.0, 0.0);
 vec3 WHITE = vec3(1.0);
@@ -20,7 +21,7 @@ float drawCircle (vec2 uv, vec2 pos, float radius){
 
 float Star14(vec2 uv, float size) {
     float a = mod( atan(uv.x,uv.y)*14.,TAU ) / TAU -PI/TAU,
-      angle = PI*(.5+.2),
+      angle = PI*.7,
           d = length(uv)*  sin(angle+abs(a)),
           w = fwidth(d);
     return length(uv)>.4 ? 0. : smoothstep(size + w, size - w, d);
@@ -32,21 +33,23 @@ vec2 Remap(vec2 p, float b, float l, float t, float r) {
 
 vec3 Flag(vec2 uv) {
 
-    vec3 col = BLUE;
-    vec2 st = Remap(uv, .384, 0.0, 1.0, 0.5);
+    float y = sin(uv.y*PI*14.);
+    float w = fwidth(y);
+    float stripes = smoothstep(-w, w, y);
+    vec3 col = mix(RED, WHITE, stripes);
+
+    vec2 st = Remap(uv, BlueHeight, 0.0, 1.0, 0.5);
     if(st.x>0. && st.x<1. && st.y>0. && st.y<1.) {
+        col = BLUE;
         vec2 stM = Remap(uv, 0.1, 0.1, 0.8, 0.5);
-        float Moon = clamp( drawCircle(stM, vec2(0.23,0.85), 0.35)-drawCircle(stM, vec2(0.31,0.85), 0.30) ,0.0,1.0);
-        vec2 stS = Remap(uv, 0.69, 0.34, 1.47, 0.8);
+        float Moon = clamp( drawCircle(stM, vec2(0.23,0.88), 0.35)-drawCircle(stM, vec2(0.31,0.88), 0.30) ,0.0,1.0);
+        vec2 stS = Remap(uv, 0.72, 0.34, 1.47, 0.78);
         float Star = Star14(stS.xy, 0.1);
         col += vec3(abs((Moon + Star))) * YELLOW;
-    } else {
-        float y = sin(uv.y*PI*13.);
-        float w = fwidth(y);
-        float stripes = smoothstep(-w, w, y);
-        w = fwidth(uv.y);
-        col = mix(WHITE, RED, stripes);
     }
+       
+    st = Remap(uv, 0.0, 0.0, 1.0, 1.0);
+    if (st.y < .0 || st.y > 1.0) col = col * vec3(0.);
     return col;
 }
 
